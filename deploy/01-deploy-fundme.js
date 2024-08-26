2c6acb77dbfaf8f807c6282f603875c6165a68c6
vikas joshi <vickyjoshi2604@gmail.com>
2024-08-26 15:16:04 -0400

    mock functionality tested

--- deploy/01-deploy-fundme.js
+++ deploy/01-deploy-fundme.js
@@ -3,13 +3,28 @@ const {
   networkConfig,
   developmentChains,
 } = require("../helper-hardhat-config");
-// const { verify } = require("../utils/verify");
 require("dotenv").config();
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
-  const { chainId } = network.config.chainId;
+  const chainId = network.config.chainId;
 
-  const ethUsdPriceFeed = networkConfig[chainId]["ethUsdPriceFeed"];
+  // const ethUsdPriceFeed = networkConfig[chainId]["ethUsdPriceFeed"];
+  let ethUsdPriceFeedAddress;
+  if (chainId == 31337) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+
+  const fundme = await deploy("Fundme", {
+    from: deployer,
+    args: [ethUsdPriceFeedAddress],
+    log: true,
+  });
+  log("--------------------------------------");
 };
+
+module.exports.tags = ["all", "fundme"];
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,19 +3,22 @@ const networkConfig = {
     name: "sepolia",
     ethUsdPriceFeed: "0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70",
   },
+  31337: {
+    name: "localhost",
+  },
   137: {
     name: "polygon",
     ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
   },
 };
 
-const developmentChain = ["hardhat", "localhost"];
+const developmentChains = ["hardhat", "localhost"];
 const DECIMALS = 8;
 const INITIAL_ANSWER = 200000000000;
 
 module.exports = {
   networkConfig,
-  developmentChain,
+  developmentChains,
   DECIMALS,
   INITIAL_ANSWER,
 };
